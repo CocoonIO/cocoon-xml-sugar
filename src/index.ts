3d99a93e7ea2c4814a5926fa4c4facfc12874241
Jorge Domínguez Arnáez (BlueSialia) <bluesialia@gmail.com>
2017-03-18 15:58:56 +0100

    Export enums too.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 "use strict";
 
 import XMLSugar from "./lib/xml-sugar";
+export {Environment} from "./lib/enums/e-environment";
+export {Orientation} from "./lib/enums/e-orientation";
 
 export default XMLSugar;
